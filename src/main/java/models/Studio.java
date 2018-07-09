cd5dab2120f225da19e132bbacfae099808a6426
Stephan Constantial <Sconstantial@yahoo.com>
2018-07-09 10:26:50 +0100

    working on Studio class

--- src/main/java/models/Studio.java
+++ src/main/java/models/Studio.java
@@ -1,17 +1,56 @@
 package models;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "studios")
 public class Studio {
     private int id;
     private String name;
     private int capacity;
+    private Producer producer;
 
     public Studio(){
 
     }
 
-    public Studio(int id, String name, int capacity) {
-        this.id = id;
+    public Studio( String name, int capacity, Producer producer) {
         this.name = name;
         this.capacity = capacity;
+        this.producer = producer;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public Producer getProducer() {
+        return producer;
+    }
+
+    public void setProducer(Producer producer) {
+        this.producer = producer;
+    }
+}
 }
